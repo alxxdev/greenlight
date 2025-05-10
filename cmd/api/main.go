356bc58ee34da2c04daa9968b60dd5d9bc0b6177
alxxdev <165938806+alxxdev@users.noreply.github.com>
2025-05-10 10:45:32 +0300

    add CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -65,6 +69,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "foo", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "foo", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "sender@example.com", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
